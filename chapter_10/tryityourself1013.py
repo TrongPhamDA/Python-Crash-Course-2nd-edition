17d1b57a986f703fb0d709192ededdecabb2db43
Trọng, Phạm <trongphamda@gmail.com>
2021-09-09 19:48:37 +0700

    try it yourself 10.13
    try it yourself 10.13

---
+++ chapter_10/tryityourself1013.py
@@ -0,0 +1,35 @@
+import json
+
+def get_stored_username():
+    filename = 'username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    """Prompt for a new username."""
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+    return username
+
+def greet_user():
+    """Greet the user by name."""
+    username = get_stored_username()
+    if username:
+        correct_user = input(f"Are you {username}? y/n\n")
+        if correct_user in ['y', 'Y']:
+            print(f"Welcome back, {username}!")
+        else:
+            username = get_new_username()
+            print(f"We'll remember you when you come back, {username}!")
+    else:
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username}!")
+
+greet_user()
